c7f6357f07873a2905f0908b4f0e48be27eb3fb7
Erin Gurley <erinmgurley@gmail.com>
2019-07-17 11:11:44 -0700

    wrote first test

---
+++ README.md
@@ -0,0 +1,12 @@
+ 1. npm init alchemy-be PROJECT_NAMEW
+
+ 1. cd into PROJECT_NAME
+
+ 1. create a new repo on github
+
+ 1. git remote add origin REPO_URL **do not initialize with README.md
+
+
+ Lab Notes
+ 
+
--- test/app.test.js
+++ test/app.test.js
@@ -17,4 +17,27 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+  
+  it('creates a new people', () => {
+    return request(app)
+      .post('/api/v1/peoples')
+      .send({ name: 'Erin',
+        city: 'Portland', 
+        state: 'Oregon',
+        email: 'erinmgurley@gmail.com', 
+        profileImage: 'https://i.ytimg.com/vi/-2cj5pPe778/maxresdefault.jpg'
+      })
+      .then(res => { {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Erin', 
+          city: 'Portland', 
+          state: 'Oregon', 
+          profileImage: 'https://i.ytimg.com/vi/-2cj5pPe778/maxresdefault.jpg', 
+          email: 'erinmgurley@gmail.com', 
+          __v:0
+        });
+      }});
+
+  });
 });
